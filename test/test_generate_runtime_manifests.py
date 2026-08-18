f478c26c42c701109f8edc66ee2d3a6bb52624b0
Lxk_buaa <867768510@qq.com>
2026-08-18 20:02:03 +0800

    Update hardcoded version checks to the bumped revision
    Compliance and unit tests still asserted the previous Debian revision.

--- test/test_generate_runtime_manifests.py
+++ test/test_generate_runtime_manifests.py
@@ -751,8 +751,8 @@ class RuntimeManifestGeneratorTest(unittest.TestCase):
         product = yaml.safe_load(
             (REPOSITORY_ROOT / ".xgc2/product.yml").read_text(encoding="utf-8")
         )
-        self.assertEqual(product["version"], "0.5.0-19")
-        self.assertEqual(product["release"]["apt_versions"]["focal"], "0.5.0-19")
+        self.assertEqual(product["version"], "0.5.0-20")
+        self.assertEqual(product["release"]["apt_versions"]["focal"], "0.5.0-20")
         self.assertNotIn(
             "xgc2-b2arx-description",
             product["release"]["dependency_policy"],
