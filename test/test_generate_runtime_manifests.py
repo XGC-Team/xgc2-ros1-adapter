87b2d9bad0c9744ee7185f95725b830151b93b91
Lxk_buaa <867768510@qq.com>
2026-08-22 04:25:28 +0800

    Release robot power telemetry adapters

--- test/test_generate_runtime_manifests.py
+++ test/test_generate_runtime_manifests.py
@@ -752,8 +752,8 @@ class RuntimeManifestGeneratorTest(unittest.TestCase):
         product = yaml.safe_load(
             (REPOSITORY_ROOT / ".xgc2/product.yml").read_text(encoding="utf-8")
         )
-        self.assertEqual(product["version"], "0.5.0-20")
-        self.assertEqual(product["release"]["apt_versions"]["focal"], "0.5.0-20")
+        self.assertEqual(product["version"], "0.5.0-21")
+        self.assertEqual(product["release"]["apt_versions"]["focal"], "0.5.0-21")
         self.assertNotIn(
             "xgc2-b2arx-description",
             product["release"]["dependency_policy"],
